0c3cb31f111e32298de3ae049171ffbdc92f6764
Sapo2700 <sapo2700@users.noreply.github.com>
2026-09-06 23:42:28 +0100

    Fix SSL: Node 18 + TLS permissivo

--- server.js
+++ server.js
@@ -39,7 +39,10 @@ async function connectDB() {
         process.exit(1);
     }
 
-    const client = new MongoClient(MONGO_URI);
+    const client = new MongoClient(MONGO_URI, {
+        ssl: true,
+        tlsAllowInvalidCertificates: true
+    });
     await client.connect();
     db = client.db('todoapp');
     users = db.collection('users');
